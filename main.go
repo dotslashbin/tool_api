0688277d6e9509fe188934291db59516ffeec34f
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-03-26 15:58:57 +0800

    test 1

--- main.go
+++ main.go
@@ -9,6 +9,6 @@ import (
 
 func main() {
 	router := router.Mux()
-	fmt.Println("webserver running ... version 2")
+	fmt.Println("webserver running ... test 1")
 	log.Fatal(http.ListenAndServe(":5000", router))
 }
